a2eceee2d282f32247a9c2f2e7652ccca0a2942a
Akash <akashsengar786@gmail.com>
2019-02-16 21:08:44 +0530

    adding files

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogPostTileComponent,
+    BlogListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
